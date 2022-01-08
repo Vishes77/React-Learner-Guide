8e50b66c94effb585689d540992398c16b7e5056
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-01-08 12:39:03 +0530

    little changes

---
+++ 13-Form-react/form-demo/src/App.js
@@ -0,0 +1,14 @@
+import Form from './from'
+import MultiForm from './multipleform'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Form/>
+        <MultiForm/>
+    </div>
+  );
+}
+
+export default App;
