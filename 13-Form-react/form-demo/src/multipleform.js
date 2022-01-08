71794ca090827c610eb5fe9aa6be6b425906d275
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-01-08 12:42:54 +0530

    ok

--- 13-Form-react/form-demo/src/multipleform.js
+++ 13-Form-react/form-demo/src/multipleform.js
@@ -1,34 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class multipleform extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name:"",
-            email: "",
-            password: "",
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-    // multiple values change here through a single function.
-    handleChange(evt){
-        this.setState({
-            [evt.target.name] : evt.target.value
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  // multiple values change here through a single function.
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
 
-    render() {
-        return(
-            <div>
-                <h1>Multi input change</h1>
-                <form action="">
-                    <input type="text" name="name" placeholder="name" onChange={this.handleChange}/>
-                    <input type="text" name="email" placeholder="email" onChange={this.handleChange}/>
-                    <input type="text" name="password" placeholder="password" onChange={this.handleChange}/>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Multi input change</h1>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default multipleform;
